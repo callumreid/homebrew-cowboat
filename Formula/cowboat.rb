bd005bbf3897d4e24450483eae8c7cf64d03fac9
callumreid <callum.reid@volleythat.com>
2025-06-06 11:47:15 -0700

    mooo new version 1.2.0

--- Formula/cowboat.rb
+++ Formula/cowboat.rb
@@ -1,10 +1,10 @@
 class Cowboat < Formula
   desc "Whimsical terminal animation of cows sailing away on boats"
   homepage "https://github.com/callumreid/cowboat"
-  url "https://github.com/callumreid/cowboat/archive/v1.1.0.tar.gz"
-  sha256 "c781f8a90a880de35b6fcb421a6a53c2fa81f3768e0c0f0e6dbee56addeb235a"
+  url "https://github.com/callumreid/cowboat/archive/v1.2.0.tar.gz"
+  sha256 "a70a766c3f0db70d69d032d264578fad377b7b505f21317ff9dae5bdfce9cca8"
   license "MIT"
-  version "1.1.0"
+  version "1.2.0"
 
   depends_on "python@3.11"
 
