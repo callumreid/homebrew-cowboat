626d70f63540521007bdd912b19d3f79dc089c56
callumreid <callum.reid@volleythat.com>
2025-06-06 11:34:26 -0700

    bump version for mini cow

--- Formula/cowboat.rb
+++ Formula/cowboat.rb
@@ -1,10 +1,10 @@
 class Cowboat < Formula
   desc "Whimsical terminal animation of cows sailing away on boats"
   homepage "https://github.com/callumreid/cowboat"
-  url "https://github.com/callumreid/cowboat/archive/v1.0.0.tar.gz"
-  sha256 "74fa4a7cfbec6df3dd01aba409dd70eb30fc9021de40bc98115d3d83f5749f62"
+  url "https://github.com/callumreid/cowboat/archive/v1.1.0.tar.gz"
+  sha256 "c781f8a90a880de35b6fcb421a6a53c2fa81f3768e0c0f0e6dbee56addeb235a"
   license "MIT"
-  version "1.0.0"
+  version "1.1.0"
 
   depends_on "python@3.11"
 
